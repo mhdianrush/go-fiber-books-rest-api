1d4407fe7314e6a60c3b1a54588be5095eb28391
mhdianrush <mhdianrush@gmail.com>
2023-07-15 11:10:35 +0700

    Created Book struct with Gorm and Json Tag

---
+++ entities/book.go
@@ -0,0 +1,9 @@
+package entities
+
+type Book struct {
+	Id          int64  `gorm:"primaryKey" json:"id"`
+	Title       string `gorm:"type:varchar(100)" json:"title"`
+	Description string `gorm:"type:text" json:"description"`
+	Author      string `gorm:"type:varchar(50)" json:"author"`
+	PublishDate string `gorm:"type:date" json:"publish_date"`
+}
