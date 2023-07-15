06a49177212e362589292d9521e0c1ba46176da3
mhdianrush <mhdianrush@gmail.com>
2023-07-15 11:12:08 +0700

    Created for Index, Find, Create, Update, and Delete Function

---
+++ controllers/book.controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func Index(c *fiber.Ctx) error {
+	return nil
+}
+
+func Find(c *fiber.Ctx) error {
+	return nil
+}
+
+func Create(c *fiber.Ctx) error {
+	return nil
+}
+
+func Update(c *fiber.Ctx) error {
+	return nil
+}
+
+func Delete(c *fiber.Ctx) error {
+	return nil
+}
