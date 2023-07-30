9de3c1f68f9f4771ba43b651d1c2f3b956cd47f3
mhdianrush <mhdianrush@gmail.com>
2023-07-30 21:33:54 +0700

    Developed for Using godotenv Package to Read env File that Used in Open Connection to MySQL Database

--- config/database.go
+++ config/database.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/mhdianrush/go-fiber-books-rest-api/entities"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
@@ -12,12 +16,20 @@ var DB *gorm.DB
 var logger = logrus.New()
 
 func ConnectDB() {
-	db, err := gorm.Open(mysql.Open("root:admin@tcp(127.0.0.1:3306)/go_fiber_books_rest_api"), &gorm.Config{})
+	if err := godotenv.Load(); err != nil {
+		logger.Printf("failed load env file %s", err.Error())
+	}
+
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
 	if err != nil {
-		logger.Println("Failed to Connect Database")
+		logger.Printf("failed connect to database %s", err.Error())
 	}
 	db.AutoMigrate(&entities.Book{})
-	
-	logger.Println("Database Connected")
+
+	logger.Println("database connected")
 	DB = db
 }
